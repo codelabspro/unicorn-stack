b8612a57a30bf7332bd95c849ceb347e54468723
arunabhdas <arunabhdas@gmail.com>
2023-08-27 01:39:47 -0400

    Added guardian and bcrypt to dependencies

--- unicorn_stack_api/mix.exs
+++ unicorn_stack_api/mix.exs
@@ -40,7 +40,9 @@ defmodule UnicornStackApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:guardian, "~> 2.3"},
+      {:bcrypt_elixir, "~> 3.0"}
     ]
   end
 
